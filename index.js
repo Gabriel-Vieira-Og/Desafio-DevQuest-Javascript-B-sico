b0777fa18f9a7fab2c92b563c9ecde373fb29cf2
Gabriel Vieira Ogioni <gabrielprojeto@gmail.com>
2022-06-13 09:32:11 -0300

    adicionado arquivo index.html e index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+
+let resultadoDaSoma = soma(10, 5)
+console.log('a soma dos 2 números é: ' + resultadoDaSoma)
+
+function divisao(numero1, numero2) {
+    return numero1 / numero2
+}
+
+let resultadoDaDivisão = divisao(10, 5)
+console.log('a divisão dos 2 números é: ' + resultadoDaDivisão)
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+let resultadoDaMultiplicação = multiplicacao(10, 5)
+console.log('a multiplicação dos 2 números é: ' + resultadoDaMultiplicação)
+
+function subtracao(numero1, numero2) {
+    return numero1 + numero2
+}
+
+let resultadoDaSubtração = subtracao(10, 5)
+console.log('a soma dos 2 números é: ' + resultadoDaSubtração)
